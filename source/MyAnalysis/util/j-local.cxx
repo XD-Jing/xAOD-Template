f600846af9552805e5cdbaa286525e9eede6442a
Jing Li <jing-li@sjtu.edu.cn>
2017-08-26 14:41:30 +0800

    parse argv

--- source/MyAnalysis/util/j-local.cxx
+++ source/MyAnalysis/util/j-local.cxx
@@ -7,10 +7,8 @@
 #include <AsgTools/MessageCheck.h>
 #include <EventLoopAlgs/NTupleSvc.h>
 #include <EventLoop/OutputStream.h>
-#include <SampleHandler/ToolsDiscovery.h>
-#include "EventLoopGrid/PrunDriver.h"
 
-void ATestRun (const std::string& submitDir, const std::string& dataset_in, const std::string& dataset_out)
+void ATestRun (const std::string& submitDir)
 {
     // Set up the job for xAOD access:
     xAOD::Init().ignore();
@@ -24,9 +22,11 @@ void ATestRun (const std::string& submitDir, const std::string& dataset_in, cons
     // that you copy all subdirectories and point this to the directory
     // containing all the files, not the subdirectories.
 
+    // use SampleHandler to scan all of the subdirectories of a directory for particular MC single file:
+    const char* inputFilePath = gSystem->ExpandPathName ("/afs/cern.ch/work/l/liji/aaa/");
+    //const char* inputFilePath = gSystem->ExpandPathName ("$ALRB_TutorialData/r9315/");
+    SH::ScanDir().filePattern("AOD.11182705._000001.pool.root.1").scan(sh,inputFilePath);
 
-    //SH::scanRucio (sh, "data16_13TeV.periodAllYear.physics_Main.PhysCont.DAOD_ZMUMU.repro21_v01/");
-    SH::scanRucio (sh, dataset_in);
 
     // set the name of the tree in our files
     // in the xAOD the TTree containing the EDM containers is "CollectionTree"
@@ -43,9 +43,9 @@ void ATestRun (const std::string& submitDir, const std::string& dataset_in, cons
     job.options()->setDouble (EL::Job::optMaxEvents, 10); // for testing purposes, limit to run over the first 500 events only!
 
     // define an output and an ntuple associated to that output
-    EL::OutputStream output  ("output");
+    EL::OutputStream output  ("myOutput");
     job.outputAdd (output);
-    EL::NTupleSvc *ntuple = new EL::NTupleSvc ("output");
+    EL::NTupleSvc *ntuple = new EL::NTupleSvc ("myOutput");
     job.algsAdd (ntuple);
 
     // add our algorithm to the job
@@ -59,18 +59,15 @@ void ATestRun (const std::string& submitDir, const std::string& dataset_in, cons
 
     job.algsAdd (alg);
 
-    alg->outputName = "output"; // give the name of the output to our algorithm
+    alg->outputName = "myOutput"; // give the name of the output to our algorithm
 
     // make the driver we want to use:
     // this one works by running the algorithm directly:
-    EL::PrunDriver driver;
-    if ( dataset_out.empty()) driver.options()->setString("nc_outputSampleName", "user.liji.test.%in:name[2]%.%in:name[6]%");
-    else driver.options()->setString("nc_outputSampleName", dataset_out);
-
+    EL::DirectDriver driver;
     // we can use other drivers to run things on the Grid, with PROOF, etc.
 
     // process the job using the driver
-    driver.submitOnly (job, submitDir);
+    driver.submit (job, submitDir);
 }
 
 int main (int argc, char* argv[])
@@ -81,27 +78,18 @@ int main (int argc, char* argv[])
 
     // Take the submit directory from the input if provided:
     std::string submitDir = "submitDir";
-    std::string dataset_out = "";
-    std::string dataset_in= "";
-    for (int i = 1;i < argc; i++){
-        if (strcmp(argv[i], "--dir")==0) submitDir = argv[i+1];
-        if (strcmp(argv[i], "--dataset")==0) dataset_in = argv[i+1];
-        if (strcmp(argv[i], "--out")==0) dataset_out = argv[i+1];
-        if (strcmp(argv[i], "--help")==0) {
-            std::cout << "j.grid usage:"<<std::endl;
-            std::cout << "   --dir [submitDir] "<<std::endl;
-            std::cout << "   --dataset [dataset] "<<std::endl;
-            std::cout << "   --out [dataset output] "<<std::endl;
-            std::cout << "   --help "<<std::endl;
-        }
+    if( argc == 2 ) submitDir = argv[1];
+    if (argc > 2)
+    {
+        ANA_MSG_ERROR ("don't know what to do with extra arguments, aborting");
+        return -1;
     }
-
 
     // Set up the job for xAOD access:
     ANA_CHECK (xAOD::Init());
 
     // call our actual job-submission code
-    ATestRun (submitDir, dataset_in, dataset_out);
+    ATestRun (submitDir);
 
     return 0;
 }
